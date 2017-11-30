c476a666598f721fc554d4cb8fec6ac771ac1824
Jens Nordenbro <jnb@beijer.se>
2017-11-30 18:31:34 +0100

    anomalipattern

--- AnomaliDevice/Program.cs
+++ AnomaliDevice/Program.cs
@@ -14,7 +14,9 @@
         private const string DeviceId = "myFirstDevice";
         //private const string DeviceKey = "TT+24WpGw+WDfSGlbhs065TFTxFD78Rx5BhAEoyAS0Q=";
         //private const string DeviceId = "anomaliDevice";
-        private const double MinTemperature = 80;
+        private const double MinTemperature = 20;
+        private const double TempDeviation = 2;
+
         private const double MinHumidity = 60;
         private static readonly Random Rand = new Random();
         private static DeviceClient _deviceClient;
@@ -24,25 +26,50 @@
         {
             while (true)
             {
-                var currentTemperature = MinTemperature + Rand.NextDouble() * 15;
-                var currentHumidity = MinHumidity + Rand.NextDouble() * 20;
-
-                var telemetryDataPoint = new
+                int normalFlow = 200;
+                Console.WriteLine("Entering normal flow for {0} iterations", normalFlow);
+                while (normalFlow-- > 0)
                 {
-                    messageId = _messageId++,
-                    deviceId = DeviceId,
-                    temperature = currentTemperature,
-                    humidity = currentHumidity
-                };
-                var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
-                var message = new Message(Encoding.ASCII.GetBytes(messageString));
-                message.Properties.Add("temperatureAlert", (currentTemperature > 30) ? "true" : "false");
 
-                await _deviceClient.SendEventAsync(message);
-                Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+                    var currentTemperature = MinTemperature + Rand.NextDouble() * TempDeviation;
+                    var currentHumidity = MinHumidity + Rand.NextDouble() * 20;
 
-                await Task.Delay(60 *1000);
+                    await SendTelemetryData(currentTemperature, currentHumidity);
+
+                    await Task.Delay(1000);
+                }
+
+                {
+                    Console.WriteLine("Doing 2 anomalies");
+                    var currentTemperature = 80;
+                    var currentHumidity = MinHumidity + Rand.NextDouble() * 20;
+                    await SendTelemetryData(currentTemperature, currentHumidity);
+                    await Task.Delay(1000);
+                    await SendTelemetryData(currentTemperature, currentHumidity);
+                    await Task.Delay(1000);
+                    await SendTelemetryData(currentTemperature, currentHumidity);
+                    await Task.Delay(1000);
+                    await SendTelemetryData(currentTemperature, currentHumidity);
+                    await Task.Delay(1000);
+                }
             }
+
+        }
+
+        private static async Task SendTelemetryData(double currentTemperature, double currentHumidity)
+        {
+            var telemetryDataPoint = new
+            {
+                messageId = _messageId++,
+                deviceId = DeviceId,
+                temperature = currentTemperature,
+                humidity = currentHumidity
+            };
+            var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+            var message = new Message(Encoding.ASCII.GetBytes(messageString));
+
+            await _deviceClient.SendEventAsync(message);
+            Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
         }
 
         private static void Main(string[] args)
