4517ce8051b1c8304303cdee8bedd0e7e039e24f
Your Name <you@example.com>
2017-12-02 16:48:47 +0100

    lib to use from scriptmodule to be able to control the mean value of temperature

---
+++ AnomalyDeviceLib/IValueContainer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnomalyDeviceLib
+{
+    public interface IValueContainer
+    {
+        float Value { get; set; }
+        float Deviation { get; set; }
+        void Pingback(double value);
+    }
+}
---
+++ AnomalyDeviceLib/Telemetry.cs
@@ -0,0 +1,70 @@
+﻿namespace SimulatedDevice
+{
+    using System;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Devices.Client;
+    using Newtonsoft.Json;
+    using AnomalyDeviceLib;
+
+    public class Telemetry
+    {
+        private const string IotHubUri = "hton.azure-devices.net";
+
+        private const string DeviceKey = "lZvsotUeMRZ8pyYM+WLxuQZDtRvMKVSckU6Gp9IBaVY=";
+        private const string DeviceId = "myFirstDevice";
+        //private const string DeviceKey = "TT+24WpGw+WDfSGlbhs065TFTxFD78Rx5BhAEoyAS0Q=";
+        //private const string DeviceId = "anomaliDevice"; // det är den andra devicen jag skapade
+        private const double MinTemperature = 20;
+        private const double TempDeviation = 2;
+
+        private const double MinHumidity = 60;
+        private static readonly Random Rand = new Random();
+        private static DeviceClient _deviceClient;
+        private static int _messageId = 1;
+
+        private static async void SendDeviceToCloudMessagesAsync(IValueContainer valueContainer)
+        {
+            while (true)
+            {
+                {
+
+                    var currentTemperature = valueContainer.Value + Rand.NextDouble() * valueContainer.Deviation;
+                    var currentHumidity = MinHumidity + Rand.NextDouble() * 20;
+                    valueContainer.Pingback(currentTemperature);
+                    await SendTelemetryData(currentTemperature, currentHumidity);
+
+                    await Task.Delay(1000);
+                }
+
+               
+            }
+
+        }
+
+        private static async Task SendTelemetryData(double currentTemperature, double currentHumidity)
+        {
+            var telemetryDataPoint = new
+            {
+                messageId = _messageId++,
+                deviceId = DeviceId,
+                temperature = currentTemperature,
+                humidity = currentHumidity
+            };
+            var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+            var message = new Message(Encoding.ASCII.GetBytes(messageString));
+
+            await _deviceClient.SendEventAsync(message);
+            Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+        }
+
+        public static void Go(IValueContainer valueContainer)
+        {
+            Console.WriteLine("Anomali device\n");
+            _deviceClient = DeviceClient.Create(IotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey(DeviceId, DeviceKey), TransportType.Mqtt);
+
+            SendDeviceToCloudMessagesAsync(valueContainer);
+            Console.ReadLine();
+        }
+    }
+}
